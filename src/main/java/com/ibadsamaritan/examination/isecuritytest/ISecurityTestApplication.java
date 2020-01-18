67cc63fc9d69549a6cbfea11abcb3b2d2ccf1587
Andrei Trukhan <andrei-truhan@tut.by>
2020-01-18 17:42:28 +0300

    Init

--- src/main/java/com/ibadsamaritan/examination/isecuritytest/ISecurityTestApplication.java
+++ src/main/java/com/ibadsamaritan/examination/isecuritytest/ISecurityTestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ISecurityTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IsecuritytestApplication.class, args);
+		SpringApplication.run(ISecurityTestApplication.class, args);
 	}
 
 }
