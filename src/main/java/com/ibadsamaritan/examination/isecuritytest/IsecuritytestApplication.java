c632133cccdf2d135234e1c45a3db276b55ff117
Andrei Trukhan <andrei-truhan@tut.by>
2020-01-18 17:40:18 +0300

    Init

---
+++ src/main/java/com/ibadsamaritan/examination/isecuritytest/IsecuritytestApplication.java
@@ -0,0 +1,13 @@
+package com.ibadsamaritan.examination.isecuritytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISecurityTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsecuritytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibadsamaritan/examination/isecuritytest/IsecuritytestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibadsamaritan.examination.isecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsecuritytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
